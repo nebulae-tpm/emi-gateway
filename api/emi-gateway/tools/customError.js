5bc4fc01481fb1994d5937b187f65a8efa5cbbe0
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-12-19 09:29:28 -0500

    added graphql response tools

--- api/emi-gateway/tools/customError.js
+++ api/emi-gateway/tools/customError.js
@@ -1,8 +1,12 @@
+//Every single error code
+const INTERNAL_SERVER_ERROR_CODE = 1;
+const PERMISSION_DENIED = 2;
+
 /**
  * class to encapsulute diferent errors.
  */
 class CustomError extends Error {
-  constructor(name, method, code , message = '') {
+  constructor(name, method, code = INTERNAL_SERVER_ERROR_CODE , message = '') {
     super(message); 
     this.code = code;
     this.name = name;
@@ -19,6 +23,25 @@ class CustomError extends Error {
   }
 };
 
+class DefaultError extends Error {
+  constructor(anyError) {
+    super(anyError.message)
+    this.code = INTERNAL_SERVER_ERROR_CODE;
+    this.name = anyError.name;
+    this.msg = anyError.message;
+    // this.stack = anyError.stack;
+  }
+
+  getContent() {
+    return {
+      code: this.code,
+      name: this.name,
+      msg: this.msg
+    }
+  }
+}
+
 module.exports =  { 
-  CustomError
+  CustomError,
+  DefaultError
 }
