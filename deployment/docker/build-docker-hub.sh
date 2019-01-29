63b68739345aa5d439ba5eaba36b92f587ad73d3
juansmolano <juansmolano@gmail.com>
2019-01-29 11:16:26 -0500

    force rebuild

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=emigateway
-export DOCKERHUB_TAG=0.0.16
+export DOCKERHUB_TAG=0.0.17
 
 rm -rf deployment/docker/emi-gateway/
 cp -R $API_SHELL_PATH/emi-gateway deployment/docker/emi-gateway
